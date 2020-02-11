5aae3ad614e2f465c83f1f8f0cb3d8a689f073fa
prashant-freelance <prashant.work.1984@gmail.com>
2020-02-11 21:32:14 +0530

    dotenv file adjusted

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+//const port = process.env.PORT || 5000;
+const morgan = require("morgan");
 
 const dotenv = require("dotenv");
 //load env variables
 dotenv.config({ path: "./config/config.env" });
 const router = express.Router();
-// use the logger middleware
-app.use(require("./middleware/logger"));
+// use the logger middleware in development mode only.
+console.log(process.env.PORT || 5000);
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 //mount the  bootcamp router file
 app.use("/api/v1/bootcamps", require("./routes/bootcamps"));
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} on PORT:${PORT}`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on PORT:${process.env.PORT ||
+      5000}`
+  );
 });
