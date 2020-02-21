414cc0b4a85e4d73c79a31bbaa00d04f62c2944a
prashant-freelance <prashant.work.1984@gmail.com>
2020-02-21 18:23:26 +0530

    Cast error for catch and duplicate key error in our error middleware

--- middleware/error.js
+++ middleware/error.js
@@ -2,16 +2,20 @@ const ErrorResponse = require("../utils/errorResponse");
 const errorHandler = (err, req, res, next) => {
   let error = { ...err };
   error.message = err.message;
-  //log to console for dev
-  //   console.log(err.stack.red);
+  //log to console for dev only while testing and you get an error code of 500
+  //   console.log(err);
 
   //mongoose bad object ID
-  //   console.log(err);
 
   if (err.name === "CastError") {
     // catch block error in bootcamp controller
     const message = `Resource with ID :${err.value} does not exist`;
     error = new ErrorResponse(message, 404);
+  }
+  //duplicate key entry error
+  if (err.code === 11000) {
+    const message = "Resource already exists";
+    error = new ErrorResponse(message, 409);
   }
   res
     .status(error.statusCode || 500)
