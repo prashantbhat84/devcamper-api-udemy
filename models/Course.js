b357fd1d9a674505280bca7b493b2c2f3bfb1c4c
prashant-freelance <prashant.work.1984@gmail.com>
2020-02-22 22:30:51 +0530

    aggregation to calculate averagecost

--- models/Course.js
+++ models/Course.js
@@ -37,5 +37,33 @@ const courseSchema = new mongoose.Schema({
     required: true
   }
 });
+courseSchema.statics.getAverageCost = async function(bootcampId) {
+  console.log("calculating average cost".blue);
+
+  const obj = await this.aggregate([
+    { $match: { bootcamp: bootcampId } },
+    {
+      $group: {
+        _id: "$bootcamp",
+        averageCost: { $avg: "$tuition" }
+      }
+    }
+  ]);
+  try {
+    await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+//call getAverageCost  after save
+courseSchema.post("save", function() {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+//call getAverageCost  before remove
+courseSchema.pre("remove", function() {
+  this.constructor.getAverageCost(this.bootcamp);
+});
 
 module.exports = mongoose.model("Course", courseSchema);
